7f4a4b15eba47933f9474316a206593832018ef2
Jahirul Islam <jahirulislam@Shafiqs-MacBook-Pro-2.local>
2022-11-25 18:41:51 +0600

    Deploy Check

--- src/Pages/Home/HomeCategory/HomeCategoryItem.js
+++ src/Pages/Home/HomeCategory/HomeCategoryItem.js
@@ -1,34 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import HomeCategory from './HomeCategory';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import HomeCategory from "./HomeCategory";
 
 const HomeCategoryItem = () => {
-    const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-    useEffect(() => {
-        fetch('http://localhost:5000/categories')
-        .then(res => res.json())
-        .then(data => setCategories(data));
-    }, [])
-    return (
-            <div className='max-w-screen-xl mx-auto py-20'>
-                <div className="categor-title text-center">
-            <h2 className='text-3xl font-bold text-red-600 mb-3'>Top Category</h2>
-            <p>We welcome feedback: you can select the flag against a sentence to report it.</p>
-            <img className='mx-auto mt-5 mb-10' src="https://i.ibb.co/Mp877Sg/separator-1.png" alt="separator-1"/>
-            </div>
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center '>
-                {
-                    categories.map(category => <Link to={`/category/${category.id}`}>
-                        <HomeCategory
-                        key={category.id}
-                        category={category}
-                    ></HomeCategory>
-                    </Link>)
-                }
-            </div>
-            </div>
-    );
+  useEffect(() => {
+    fetch("https://car-shop-server.vercel.app/categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+  return (
+    <div className="max-w-screen-xl mx-auto py-20">
+      <div className="categor-title text-center">
+        <h2 className="text-3xl font-bold text-red-600 mb-3">Top Category</h2>
+        <p>
+          We welcome feedback: you can select the flag against a sentence to report it.
+        </p>
+        <img
+          className="mx-auto mt-5 mb-10"
+          src="https://i.ibb.co/Mp877Sg/separator-1.png"
+          alt="separator-1"
+        />
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center ">
+        {categories.map((category) => (
+          <Link to={`/category/${category.id}`}>
+            <HomeCategory key={category.id} category={category}></HomeCategory>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default HomeCategoryItem;
--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -7,46 +7,47 @@ import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
 import Products from "../../Pages/Products/Products/Products";
 import SignUp from "../../Pages/SignUp/SignUp";
- 
+
 const router = createBrowserRouter([
-   {
-       path: "/",
-       element: <Main></Main>,
-       children: [
-        {
-            path: "/",
-            element: <Home></Home>
-        },
-        {
-            path: "/blog",
-            element: <Blog></Blog>
-        },
-        {
-            path: "/login",
-            element: <Login></Login>
-        },
-        {
-            path: "signup",
-            element: <SignUp></SignUp>,
-        },
-        {
-            path: "/category/:id",
-            element: <Category></Category>,
-            loader: ({params}) => fetch(`http://localhost:5000/category/${params.id}`)
-        },
-        {
-            path: "/products/:id",
-            element: <Products></Products>,
-            loader: ({params}) => fetch(`http://localhost:5000/products/${params.id}`)
-        },
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/blog",
+        element: <Blog></Blog>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "signup",
+        element: <SignUp></SignUp>,
+      },
+      {
+        path: "/category/:id",
+        element: <Category></Category>,
+        loader: ({ params }) =>
+          fetch(`https://car-shop-server.vercel.app/category/${params.id}`),
+      },
+      {
+        path: "/products/:id",
+        element: <Products></Products>,
+        loader: ({ params }) =>
+          fetch(`https://car-shop-server.vercel.app/products/${params.id}`),
+      },
 
-        {
-            path: "*",
-            element: <Error></Error>
-        },
-
-       ]
-   }
+      {
+        path: "*",
+        element: <Error></Error>,
+      },
+    ],
+  },
 ]);
- 
+
 export default router;
