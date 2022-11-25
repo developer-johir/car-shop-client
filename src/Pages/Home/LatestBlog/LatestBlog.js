41a659d02c68ab9746db1e67c75ffb75c15521cb
Jahirul Islam <jahirulislam@Shafiqs-MacBook-Pro-2.local>
2022-11-26 00:33:50 +0600

    Load category product data from MongoDB

--- src/Pages/Home/LatestBlog/LatestBlog.js
+++ src/Pages/Home/LatestBlog/LatestBlog.js
@@ -1,8 +1,9 @@
 import React from "react";
+import { FaCalendarAlt, FaUser, FaComment } from "react-icons/fa";
 
 const LatestBlog = () => {
   return (
-    <div className="max-w-screen-xl mx-auto py-20">
+    <div className="max-w-screen-xl mx-auto py-20 px-7">
       <div className="categor-title text-center">
         <h2 className="text-3xl font-bold text-red-600 mb-3">Read our latest blog</h2>
         <h2 className="text-5xl font-bold mb-3">LATEST BLOG</h2>
@@ -22,8 +23,22 @@ const LatestBlog = () => {
             <img src="https://carzone.dexignlab.com/xhtml/car-shop/images/blog/pic1.jpg" alt="Shoes" />
           </figure>
           <div className="card-body">
-            <h2 className="card-title text-red-600">Let’s Clear The Air About K...</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <h2 className="card-title text-2xl text-red-600">Let’s Clear The Air About K...</h2>
+            <div className="flex">
+              <div className="flex items-center text-lg">
+                <FaCalendarAlt></FaCalendarAlt>
+              <p className="ml-1">27 Mar 2021</p>
+              </div>
+              <div className="flex items-center ml-3 text-lg">
+                <FaUser></FaUser>
+              <p className="ml-1">By Jone</p>
+              </div>
+              <div className="flex items-center ml-3 text-lg">
+                <FaComment></FaComment>
+              <p className="ml-1">83</p>
+              </div>
+            </div>
+            <p className="text-[16px]">How long can you leave your car running with the air conditioner on before turning...</p>
             <div className="card-actions justify-start">
             <button className="btn bg-blue-700 text-white rounded-lg border-none mt-5">Read More</button>
             </div>
@@ -31,25 +46,53 @@ const LatestBlog = () => {
         </div>
         <div className="card card-compact w-96 bg-base-100 shadow-xl">
           <figure>
-            <img src="https://carzone.dexignlab.com/xhtml/car-shop/images/blog/pic2.jpg" alt="Shoes" />
+            <img src="https://carzone.dexignlab.com/xhtml/car-shop/images/blog/pic1.jpg" alt="Shoes" />
           </figure>
           <div className="card-body">
-            <h2 className="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Buy Now</button>
+            <h2 className="card-title text-2xl text-red-600">Let’s Clear The Air About K...</h2>
+            <div className="flex">
+              <div className="flex items-center text-lg">
+                <FaCalendarAlt></FaCalendarAlt>
+              <p className="ml-1">27 Mar 2021</p>
+              </div>
+              <div className="flex items-center ml-3 text-lg">
+                <FaUser></FaUser>
+              <p className="ml-1">By Jone</p>
+              </div>
+              <div className="flex items-center ml-3 text-lg">
+                <FaComment></FaComment>
+              <p className="ml-1">83</p>
+              </div>
+            </div>
+            <p className="text-[16px]">How long can you leave your car running with the air conditioner on before turning...</p>
+            <div className="card-actions justify-start">
+            <button className="btn bg-blue-700 text-white rounded-lg border-none mt-5">Read More</button>
             </div>
           </div>
         </div>
         <div className="card card-compact w-96 bg-base-100 shadow-xl">
           <figure>
-            <img src="https://carzone.dexignlab.com/xhtml/car-shop/images/blog/pic3.jpg" alt="Shoes" />
+            <img src="https://carzone.dexignlab.com/xhtml/car-shop/images/blog/pic1.jpg" alt="Shoes" />
           </figure>
           <div className="card-body">
-            <h2 className="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Buy Now</button>
+            <h2 className="card-title text-2xl text-red-600">Let’s Clear The Air About K...</h2>
+            <div className="flex">
+              <div className="flex items-center text-lg">
+                <FaCalendarAlt></FaCalendarAlt>
+              <p className="ml-1">27 Mar 2021</p>
+              </div>
+              <div className="flex items-center ml-3 text-lg">
+                <FaUser></FaUser>
+              <p className="ml-1">By Jone</p>
+              </div>
+              <div className="flex items-center ml-3 text-lg">
+                <FaComment></FaComment>
+              <p className="ml-1">83</p>
+              </div>
+            </div>
+            <p className="text-[16px]">How long can you leave your car running with the air conditioner on before turning...</p>
+            <div className="card-actions justify-start">
+            <button className="btn bg-blue-700 text-white rounded-lg border-none mt-5">Read More</button>
             </div>
           </div>
         </div>
