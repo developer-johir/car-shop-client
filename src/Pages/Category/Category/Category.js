ea416ab86aa6b87ddb092c967d15516cba8120b9
Jahirul Islam <jahirulislam@Shafiqs-MacBook-Pro-2.local>
2022-11-25 23:42:25 +0600

    Category Product Page Design Complete

--- src/Pages/Category/Category/Category.js
+++ src/Pages/Category/Category/Category.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
-import Products from '../../Products/Products/Products';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import Products from "../../Products/Products/Products";
 
 const Category = () => {
-    const categoryNews = useLoaderData();
-    return (
-        <div className='max-w-screen-xl mx-auto py-20'>
-            <h1>This is Category: {categoryNews.length}</h1>
-            {
-                categoryNews.map(product => <Products 
-                    key={product._id}
-                    product={product}
-                    >
-                </Products>)
-            }
-        </div>
-    );
+  const categoryNews = useLoaderData();
+  return (
+    <div className="max-w-screen-xl mx-auto pb-20">
+      <div className="text-center">
+      <h1 className="text-center text-3xl font-bold text-red-600 mt-14 mb-3">
+        Second-hand Products
+      </h1>
+      <p>We welcome feedback: you can select the flag against a sentence to report it.</p>
+      <img
+        className="mx-auto mt-5 mb-10"
+        src="https://i.ibb.co/Mp877Sg/separator-1.png"
+        alt="separator-1"
+      />
+      </div>
+      {categoryNews.map((product) => (
+        <Products key={product._id} product={product}></Products>
+      ))}
+    </div>
+  );
 };
 
 export default Category;
